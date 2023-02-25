a6d361c400ba6e32c027cacf5ca07908f39900b1
Noiziee <dima.maskva.1194@gmail.com>
2023-02-26 00:11:33 +0300

    fix(burger-menu): add javascript

--- src/js/main.js
+++ src/js/main.js
@@ -25,10 +25,49 @@ const swiper = new Swiper('.reviews__slider', {
   },
 });
 
-// off scroll
-document.querySelector('#burger-menu__toggle').addEventListener('change', function (e) {
-  document.body.style.overflow = e.target.checked === true ? 'hidden' : '';
-});
+// // off scroll
+// document.querySelector('#burger-menu__toggle').addEventListener('change', function (e) {
+//   document.body.style.overflow = e.target.checked === true ? 'hidden' : '';
+// });
 
+// Burger
 
+const menu = document.querySelector('.nav')
+const menuBtn = document.querySelector('.burger-menu__btn')
+
+const body = document.body;
+
+if (menu && menuBtn) {
+  menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('active')
+    menuBtn.classList.toggle('active')
+    body.classList.toggle('lock')
+
+  })
+
+  menu.querySelectorAll('.nav__link').forEach(link => {
+    link.addEventListener('click', () => {
+      menu.classList.remove('active')
+      menuBtn.classList.remove('active')
+      body.classList.remove('lock')
+    })
+  })
+}
+
+// ================================================
+
+const anchors = document.querySelectorAll('a[href*="#"')
+
+anchors.forEach(anchor => {
+  anchor.addEventListener('click', event => {
+    event.preventDefault();
+
+    const blockID = anchor.getAttribute('href').substring(1)
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+})
 
